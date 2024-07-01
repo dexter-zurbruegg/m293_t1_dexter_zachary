2f791e70f40f9c55ecf2a81222ae06befd32184e
rei905 <dexter.zurbruegg@tie-international.com>
2024-07-01 11:17:54 +0200

    log in

--- Projekt/Formulare/Site/form-scripts.js
+++ Projekt/Formulare/Site/form-scripts.js
@@ -2,3 +2,8 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     event.preventDefault();
     window.location.href = 'confirmation.html';
 });
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    window.location.href = 'main-content.html';
+});
