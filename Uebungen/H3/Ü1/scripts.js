3be37fe17309c9248a107cc71ce04555259065fc
rei905 <dexter.zurbruegg@tie-international.com>
2024-07-01 08:31:31 +0200

    formular spiele

--- Uebungen/H3/Ü1/scripts.js
+++ Uebungen/H3/Ü1/scripts.js
@@ -30,3 +30,20 @@ document.querySelectorAll('input[name="regularSpiele"]:not(#regularSpielNone)').
         }
     });
 });
+
+document.getElementById('surveyForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const output = {};
+    formData.forEach((value, key) => {
+        if (output[key]) {
+            if (!Array.isArray(output[key])) {
+                output[key] = [output[key]];
+            }
+            output[key].push(value);
+        } else {
+            output[key] = value;
+        }
+    });
+    document.getElementById('formData').textContent = JSON.stringify(output, null, 2);
+});
